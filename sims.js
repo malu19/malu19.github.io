7465e7b69323c127035c2637f4d653f033bc0ac5
malu19 <57542396+malu19@users.noreply.github.com>
2020-12-06 19:49:12 -0300

    Update sims.js

--- sims.js
+++ sims.js
@@ -6,7 +6,7 @@ botao1.onclick = function transformação(){
 	var gramas = 55.8;
 	var result = (massa/gramas);
 	result = result.toFixed(2);
-	var resultado = "1 mol - 55,8 g <br> X mol - " + massa +" g <br> <br> 55,8 . X = 1 . " + massa + " <br> X = " + massa + "/" + " 55,8 <br> X = " + resultado + " mol(s). <br><br> " + massa + " g = " + resultado + " mol(s)";
+	var resultado = "1 mol - 55,8 g <br> X mol - " + massa +" g <br> <br> 55,8 . X = 1 . " + massa + " <br> X = " + massa + "/" + " 55,8 <br> X = " + result + " mol(s). <br><br> " + massa + " g = " + result + " mol(s)";
 	document.getElementById('resultado').innerHTML = resultado;
 
 
@@ -20,7 +20,7 @@ botao2.onclick = function transformação2(){
 	var gramas = 55.8;
 	var result = (gramas*mol);
 	resultado = result.toFixed(2);
-	var resultado = "55,8 g - 1 mol <br> X g - " + mol + " mol <br><br> 1 . X = 55,8 . " + mol + " <br> X = " + gramas*mol + "/1 <br> " + "X = " + resultado + " g <br><br> " + mol + " mol = " + resultado + " g";
+	var resultado = "55,8 g - 1 mol <br> X g - " + mol + " mol <br><br> 1 . X = 55,8 . " + mol + " <br> X = " + gramas*mol + "/1 <br> " + "X = " + result + " g <br><br> " + mol + " mol = " + result + " g";
 	document.getElementById("resultado2").innerHTML = resultado;
 
 }
